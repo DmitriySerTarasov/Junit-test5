d46677d5b03b586a56ce87d32f08190d6f395363
Dmitriy Tarasov <dmitriy.tarasov@itv.ru>
2025-04-23 13:22:57 +0300

    Initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Calculator {
+
+    // Метод для сложения
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Метод для вычитания
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Метод для умножения
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Метод для деления
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Деление на ноль невозможно.");
+        }
+        return (double) a / b;
+    }
+}
